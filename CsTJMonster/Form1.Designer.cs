c5eedb49e225b1f8670c85eb535cc64cce43aadd
Raiiwhen <masterraii@icloud.com>
2020-04-02 02:58:53 +0800

    random Rx & Tx done.

---
+++ CsTJMonster/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace CsTJMonster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button_OpenCOM = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button_Sync = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_OpenCOM
+            // 
+            this.button_OpenCOM.BackColor = System.Drawing.Color.LightGray;
+            this.button_OpenCOM.Location = new System.Drawing.Point(17, 12);
+            this.button_OpenCOM.Name = "button_OpenCOM";
+            this.button_OpenCOM.Size = new System.Drawing.Size(111, 83);
+            this.button_OpenCOM.TabIndex = 5;
+            this.button_OpenCOM.Text = "Open";
+            this.button_OpenCOM.UseVisualStyleBackColor = false;
+            this.button_OpenCOM.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 140);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(318, 108);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 254);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(318, 36);
+            this.textBox2.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.radioButton5);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(130, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(132, 108);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "No COM";
+            this.label1.Visible = false;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(4, 83);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(36, 19);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "-";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(5, 64);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(36, 19);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "-";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(5, 45);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(36, 19);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "-";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(5, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(36, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "-";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(5, 7);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(36, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "-";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 97);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(111, 23);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // button_Sync
+            // 
+            this.button_Sync.Location = new System.Drawing.Point(12, 296);
+            this.button_Sync.Name = "button_Sync";
+            this.button_Sync.Size = new System.Drawing.Size(48, 44);
+            this.button_Sync.TabIndex = 9;
+            this.button_Sync.Text = "sync";
+            this.button_Sync.UseVisualStyleBackColor = true;
+            this.button_Sync.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(66, 296);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 44);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Read";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(120, 296);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 44);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "draw";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(174, 296);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 44);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(228, 296);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(48, 44);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(282, 296);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(48, 44);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel5});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 352);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(804, 25);
+            this.statusStrip1.TabIndex = 15;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(38, 20);
+            this.toolStripStatusLabel1.Text = "RTC";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(39, 20);
+            this.toolStripStatusLabel2.Text = "IMU";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(55, 20);
+            this.toolStripStatusLabel3.Text = "NAND";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(29, 20);
+            this.toolStripStatusLabel4.Text = "SD";
+            this.toolStripStatusLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(42, 20);
+            this.toolStripStatusLabel5.Text = "W25";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(336, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(456, 328);
+            this.chart1.TabIndex = 16;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 377);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button_Sync);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_OpenCOM);
+            this.Name = "Form1";
+            this.Text = "TJMonster - 光赛B题上位机";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_OpenCOM;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_Sync;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ CsTJMonster/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace CsTJMonster
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort obj;
+        public List<byte> RX_Buffer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RX_Buffer = new List<byte>(1024);
+            comboBox1.Items.Add(115200);
+            comboBox1.Items.Add(9600);
+            comboBox1.Items.Add(230400);
+            comboBox1.SelectedIndex = 0;
+            scan_valid_serial();
+            open_serial();
+            status_ask();
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = obj;
+            int bytes2read = sp.BytesToRead;
+            byte[] buffer = new byte[bytes2read];
+
+            sp.Read(buffer,0,bytes2read);
+            RX_Buffer.AddRange(buffer);
+
+            textBox1.Text += BitConverter.ToString(buffer);
+        }
+
+        private void scan_valid_serial()
+        {
+            string[] com_list = new string[5];
+            string buffer;
+            int valid_cnt = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                try
+                {
+                    buffer = "COM" + i.ToString();
+                    SerialPort list_port = new SerialPort();
+                    list_port.PortName = buffer;
+                    list_port.Open();
+                    list_port.Close();
+                    Console.WriteLine(buffer);
+                    if (valid_cnt < 5)
+                    {
+                        com_list[valid_cnt++] = buffer;
+                    }
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            radioButton1.Text = com_list[0]; radioButton1.Visible = com_list[0] == null ? false : true;
+            radioButton2.Text = com_list[1]; radioButton2.Visible = com_list[1] == null ? false : true;
+            radioButton3.Text = com_list[2]; radioButton3.Visible = com_list[2] == null ? false : true;
+            radioButton4.Text = com_list[3]; radioButton4.Visible = com_list[3] == null ? false : true;
+            radioButton5.Text = com_list[4]; radioButton5.Visible = com_list[4] == null ? false : true;
+            if(com_list[0] == null)
+            {
+                label1.Visible = true;
+            }
+            else
+            {
+                label1.Visible = false;
+            }
+
+        }
+
+        private void open_serial()
+        {
+            string current_port = "COM3";
+
+            /*串口属性选择*/
+            if (radioButton1.Checked) current_port = radioButton1.Text;
+            if (radioButton2.Checked) current_port = radioButton2.Text;
+            if (radioButton3.Checked) current_port = radioButton3.Text;
+            if (radioButton4.Checked) current_port = radioButton4.Text;
+            if (radioButton5.Checked) current_port = radioButton5.Text;
+            try
+            {
+                obj = new SerialPort(current_port);
+
+                obj.BaudRate = int.Parse(comboBox1.Text);
+                obj.Parity = Parity.None;
+                obj.StopBits = StopBits.One;
+                obj.DataBits = 8;
+                obj.Handshake = Handshake.None;
+                obj.RtsEnable = true;
+                /*注册串口事件*/
+                obj.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+                /*开串口*/
+                obj.Open();
+                button_OpenCOM.BackColor = Color.PaleTurquoise;
+                button_OpenCOM.Text = "Close";
+            }
+            catch
+            {
+                button_OpenCOM.BackColor = Color.Lime;
+                button_OpenCOM.Text = "Error";
+                Console.WriteLine("Serial Error.");
+            }
+        }
+
+        private void close_serial()
+        {
+            try
+            {
+                obj.Close();
+                button_OpenCOM.BackColor = Color.LightGray;
+                button_OpenCOM.Text = "Open";
+            }
+            catch
+            {
+                button_OpenCOM.BackColor = Color.Lime;
+                button_OpenCOM.Text = "Error";
+                Console.WriteLine("Serial Error.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button_OpenCOM.Text == "Close")
+            {
+                close_serial();
+                scan_valid_serial();
+            }
+            else
+            {
+                scan_valid_serial();
+                open_serial();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (status_update())
+            {
+                button_Sync.BackColor = Color.PaleTurquoise;
+            }
+            else
+            {
+                button_Sync.BackColor = Color.Lime;
+            }
+            status_ask();
+        }
+
+        private bool status_update()
+        {
+            string tmp;
+            bool format_check = (RX_Buffer[0] == 0xb1) && (RX_Buffer[1] == 0xb1);
+            if (format_check)
+            {
+                toolStripStatusLabel1.Text =
+                    "20" + RX_Buffer[2].ToString() +
+                    '/' + RX_Buffer[3].ToString() +
+                    '/' + RX_Buffer[4].ToString() +
+                    ' ' + RX_Buffer[5].ToString() +
+                    ':' + RX_Buffer[6].ToString() +
+                    " |";
+
+                toolStripStatusLabel2.Text = (RX_Buffer[7] == 0x01 ? "MPU9250" : "IMU offline") + " |";
+
+                switch (RX_Buffer[8])
+                {
+                    case 0x00: tmp = "NAND offline";break;
+                    case 0x01: tmp = "SAMSUNG K9 2GB " + (RX_Buffer[9] / 256).ToString() + '%'; break;
+                    case 0x02: tmp = "SAMSUNG K9 4GB " + (RX_Buffer[9] / 256).ToString() + '%'; break;
+                    default: tmp = "NAND error";break;
+                }
+                toolStripStatusLabel3.Text = tmp + " |";
+
+                switch (RX_Buffer[10])
+                {
+                    case 0x00: tmp = "SD offline"; break;
+                    case 0x01: tmp = "SD Kingston" + (RX_Buffer[11] / 256).ToString() + '%'; break;
+                    default: tmp = "SD error"; break;
+                }
+                toolStripStatusLabel4.Text = tmp + " |";
+
+                switch (RX_Buffer[12])
+                {
+                    case 0x00: tmp = "W25 offline"; break;
+                    case 0x01: tmp = "W25M02GV " + (RX_Buffer[13] / 256).ToString() + '%'; break;
+                    default: tmp = "W25 error"; break;
+                }
+                toolStripStatusLabel5.Text = tmp;
+            }
+            RX_Buffer.Clear();
+            return format_check;
+        }
+
+        private void status_ask()
+        {
+            byte[] mst_echo = new byte[3] { 0x08, 0x0a, 0x0d };
+            obj.Write(mst_echo, 0, 3);
+            textBox2.AppendText("echo_cmd\r\n");
+        }
+
+        private void data_stream()
+        {
+            byte[] mst_echo = new byte[3] { 0x09, 0x0a, 0x0d };
+            obj.Write(mst_echo, 0, 3);
+            textBox2.AppendText("data_stream\r\n");
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            data_stream();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
